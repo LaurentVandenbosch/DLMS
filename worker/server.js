33e77fbab3fce048de36140b71b9a37524661cfe
Michael Leung <maikeru.leung@gmail.com>
2018-10-28 17:35:48 +0100

    implemented mongodb messages storage

--- worker/server.js
+++ worker/server.js
@@ -1,16 +1,37 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib/callback_api');
+const amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function(err, conn) {
-  conn.createChannel(function(err, ch) {
-    var q = 'hello';
+const MongoClient = require('mongodb').MongoClient;
+const host = process.env.MONGO_HOST || 'localhost';
+const url = 'mongodb://' + host + ':27017';
 
-    ch.assertQueue(q, {durable: false});
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-    ch.consume(q, function(msg) {
-      console.log(" [x] Received %s", msg.content.toString());
-      ch.ack(msg);
-    }, {noAck: false});
+const assert = require('assert');
+
+const dbName = 'DLMS'
+
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(dbName);
+
+  amqp.connect('amqp://localhost', function(err, conn) {
+    conn.createChannel(function(err, ch) {
+      const q = 'hello';
+
+      ch.assertQueue(q, {durable: false});
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+      ch.consume(q, function(msg) {
+        console.log(" [x] Received %s", msg.content.toString());
+        db.collection(q).insertOne({"payload":msg.content.toString()});
+        ch.ack(msg);
+      }, {noAck: false});
+    });
   });
-});
+ });
+
+
+
+
+
