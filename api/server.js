17bcfae84b57573f50a7bee64e10b502ad361653
Laurent Vandenbosch <laurentvandenbosch@gmail.com>
2018-11-03 17:30:19 +0100

    Implemented simple api

---
+++ api/server.js
@@ -0,0 +1,45 @@
+'use strict';
+const { MongoClient } = require('mongodb');
+const host = process.env.MONGO_HOST || 'localhost';
+const url = 'mongodb://' + host + ':27017';
+const dbName = 'DLMS';
+const collectionName = 'messages';
+
+const Hapi = require('hapi');
+
+// Create a server with a host and port
+const server = Hapi.server({
+    host: 'localhost',
+    port: 8000
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path: '/messages',
+    handler: function (request, h) {
+        return new Promise((resolve) => {
+            MongoClient.connect(url, function(err, client) {
+                const collection = client.db(dbName).collection(collectionName);
+                resolve(collection.find().toArray());
+            });
+        });
+        
+    }
+});
+
+// Start the server
+async function start() {
+
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
