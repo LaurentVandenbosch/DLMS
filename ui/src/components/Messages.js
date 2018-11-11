7703308372141279e18fb22ba297760fa7be3164
Laurent Vandenbosch <laurentvandenbosch@gmail.com>
2018-11-11 11:02:17 +0100

    Install Grommet and cleanup

--- ui/src/components/Messages.js
+++ ui/src/components/Messages.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import {DataTable} from 'grommet';
 
 export default function Messages(props) {
+    const columns = [
+        {
+          property: "_id",
+          header: "Id",
+          primary: true
+        },
+        {
+            property: "queue",
+            header: "Queue",
+        },
+        {
+          property: "payload",
+          header: "Payload"
+        }
+    ];
+
     return (
-        <ul>
-            {
-                props.messages.map(m => 
-                    <li key={m._id}>{m._id}</li>  
-                )
-            }
-        </ul>
+        <DataTable columns={columns} data={props.messages} />
     );
 }
